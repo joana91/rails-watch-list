01a8907b3eca531fdcfa91aaf1627cb69c49551f
Joana Mena <joanamacarenamg@gmail.com>
2022-05-19 14:56:51 +0200

    Just pending bookmarks destroy

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,42 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def show
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save!
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark.list.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating ...'
+5.times do
+  puts 'starting seed'
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.paragraphs(number: 10).join(' '),
+    poster_url: Faker::Internet.url,
+    rating: Faker::Number.between(from: 0.0, to: 10.0)
+  )
+
+  movie.save
+  p movie
+end
